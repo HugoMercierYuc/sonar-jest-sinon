92d38db370ca3970ce7eeaef8b1d3a7f79c8ce3b
hugo <hugo.paris19@gmail.com>
2023-11-02 15:00:34 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'org';
+  
+  action() : void {
+    console.log('action');
+  }
+}
